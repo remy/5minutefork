f399fb1caf1925be6bf309de06ac3dc8ccb289ae
Remy Sharp <remy@remysharp.com>
2013-03-19 23:04:34 +0000

    add user tools and added the icon to background

--- public/chrome-extension/inpage.js
+++ public/chrome-extension/inpage.js
@@ -3,11 +3,7 @@
 var pageHeaderMatches = document.querySelectorAll(".pagehead-actions");
 if(pageHeaderMatches && pageHeaderMatches.length)
 {
-	var cloneURL = document.querySelectorAll(".clone-url input");
-	if(cloneURL && cloneURL.length)
-		cloneURL = cloneURL[0].value;
-	else
-		cloneURL = document.location.href;
+	var cloneURL = document.location.href;
 
 	var reResult = new RegExp("^.*?github.com[/:]([^/]+)/(.*?)(.git)?$").exec(cloneURL);
 	var fiveMinForkURL = "http://5minfork.com/" + reResult[1] + "/" + reResult[2].split('/')[0];
@@ -21,7 +17,7 @@ if(pageHeaderMatches && pageHeaderMatches.length)
 
 	var fiveMinForkButtonIcon = document.createElement("span");
 	fiveMinForkButtonIcon.className = "mini-icon";
-	fiveMinForkButtonIcon.style.backgroundImage = "url(https://assets.github.com/images/icons/emoji/clock5.png)";
+	fiveMinForkButtonIcon.style.backgroundImage = "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAA20lEQVQYGXXBQSuDYQAA4GeKi5NE5CbNTrRSCoVQWotykpOUub4ncSESh5U4ShnlB3B0Ui5a0nKQloN2wN9g9n1Zsueh3rR9B8Y1dIGEcw1lHTk26x8dJlQFAfPWZdWZU5FTFQSDtrAtKbLkWUpNEHS7NOVSmx99PiTFgiDt0aR2kUN7fgUbStJq+q3xYlRsQd6ZU7EZ93xKihWQUBAbUqZoRmzMjl3DYhkPbLrSyLUcLcoy/pP1rtW3ERWr/lr0JiXS49aNZV1o1mlF0Z1edZpMyXvy5lXJiQGRL98/MTvJIbx7AAAAAElFTkSuQmCC)";
 	fiveMinForkButtonIcon.style.backgroundSize = "16px";
 	fiveMinForkButtonIcon.style.backgroundRepeat = "no-repeat";
 
