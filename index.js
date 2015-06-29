1f07e81cc58ded6291c1f758251aff609f3816f9
Jake Champion <jakechampion.jake2@googlemail.com>
2015-06-29 10:17:22 +0100

    Use a github token instead of basic auth

--- index.js
+++ index.js
@@ -106,8 +106,10 @@ var app = connect().use(connect.logger('dev')).use(connect.favicon(__dirname + '
           });
         } else {
           request('https://api.github.com/repos/' + fork.urlWithoutBranch.join('/'), {
-            'auth': credentials,
-            'headers': { 'user-agent': '5minfork - http://5minfork.com' }
+            'headers': {
+              'user-agent': '5minfork - http://5minfork.com',
+              'Authorization': 'token ' + credentials.githubToken
+            }
           }, function (e, r, body) {
             fork.gitdata = JSON.parse(body);
             fork.repo = fork.gitdata.git_url;
